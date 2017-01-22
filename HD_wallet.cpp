6d988aeba5346fc36c9a1d653ca21ed3a704d31c
Aaron jaramillo <alpha@Aarons-MacBook-Pro.local>
2017-01-21 22:53:34 -0700

    Seperation of classes

--- HD_wallet.cpp
+++ HD_wallet.cpp
@@ -1,3 +1,4 @@
+
 #include <bitcoin/bitcoin.hpp>
 #include <string.h>
 #include <iostream>
@@ -9,9 +10,11 @@ class HD_Wallet
 public:
 
 	//constructor
-	HD_Wallet(const data_chunk seed)
+	HD_Wallet(const data_chunk Userentropy)
 	{
-		mnemonic = wallet::create_mnemonic(seed);
+		entropy = Userentropy;
+		mnemonic = wallet::create_mnemonic(entropy);
+		seed = to_chunk(wallet::decode_mnemonic(mnemonic));
 		privateKey = wallet::hd_private(seed);
 		publicKey = privateKey.to_public();
 	}
@@ -19,24 +22,34 @@ public:
 	HD_Wallet(const wallet::word_list mnemonicSeed)
 	{
 		seed = to_chunk(wallet::decode_mnemonic(mnemonicSeed));
-
+		//seed = to_chunk(hashSeed);
 		mnemonic = mnemonicSeed;
 		privateKey = wallet::hd_private(seed);
 		publicKey = privateKey.to_public();
 	}
 
 	//display output
+	void dumpKeys()
+	{
+		displayMnemonic();
+		displayPrivateKey();
+		displayChildPrivateKey(1);
+		displayAddress(1);
+
+	}
 	void displayPrivateKey()
 	{
-		std::cout << privateKey.encoded() << "\n" << std::endl;
+		std::cout << "\nPrivate Key:" << privateKey.encoded() << std::endl;
 	}
 
 	void displayMnemonic()
 	{
 		if(wallet::validate_mnemonic(mnemonic))
 		{
-			for (auto i = mnemonic.begin(); i != mnemonic.end(); ++i)
-	    		std::cout << *i << ' ';
+			std::string mnemonicString = join(mnemonic);
+			std::cout << "\n" << mnemonicString << std::endl;
+			// for (auto i = mnemonic.begin(); i != mnemonic.end(); ++i)
+	  //   		std::cout << *i << ' ';
 		}else{
 			std::cout << "mnemonic invalid!" << std::endl;
 		}
@@ -50,6 +63,14 @@ public:
 	void displayAddress(int index)
 	{
 		std::cout << "\nAddress: " << childAddress(index).encoded() << std::endl;
+	}
+	void addressRange(int start, int end)
+	{
+		while(start != end)
+		{
+			displayAddress(start);
+			start++;
+		}
 	}
 
 	//accesor
@@ -70,6 +91,7 @@ public:
 
 private:
 	//members
+	data_chunk entropy;
 	data_chunk seed;
 	wallet::word_list mnemonic;
 	wallet::hd_private privateKey;
@@ -77,95 +99,3 @@ private:
 
 
 };
-void mainMenu()
-{
-	std::cout << "==========================" << std::endl;
-	std::cout << "1. Generate New Wallet" << std::endl;
-	std::cout << "2. Import Wallet" << std::endl;
-	std::cout << "3. display" << std::endl;
-	std::cout << "6. Exit" << std::endl;
-	std::cout << "==========================" << std::endl;
-}
-
-void displayMenu()
-{
-	std::cout << "==========================" << std::endl;
-	std::cout << "1. Private Key" << std::endl;
-	std::cout << "2. Mnemonic" << std::endl;
-	std::cout << "3. Child Private Key" << std::endl;
-	std::cout << "4. Payment Address" << std::endl;
-	std::cout << "5. Exit" << std::endl;
-	std::cout << "==========================" << std::endl;
-}
-
-HD_Wallet randomGen()
-{
-	data_chunk seedChunk = data_chunk(16);
-	pseudo_random_fill(seedChunk);
-	return HD_Wallet(seedChunk);
-}
-int getInput()
-{
-	int choice;
-	std::cout << "Enter a Number: "; 
-	std::cin >> choice;
-	return choice;
-}
-wallet::word_list getWordList()
-{
-	std::string mnemonicSeed;
-	std::cout << "Enter Seed String:";
-	cin.ignore();
-	std::getline(cin, mnemonicSeed);
-	return split(mnemonicSeed);
-}
-int main(){
-	HD_Wallet wallet = randomGen();
-	int choice = 0;
-	while(choice != 6)
-	{
-		mainMenu();
-		choice = getInput();
-		if(choice == 1)
-		{	
-			wallet = randomGen();
-		} else if(choice == 2)
-		{	
-			wallet::word_list mnemonicSeedList = getWordList();
-			if(wallet::validate_mnemonic(mnemonicSeedList))
-			{
-				wallet = HD_Wallet(mnemonicSeedList);
-			} else {
-				std::cout << "Mnemonic Invalid!" << std::endl;
-			}
-
-		} else if (choice == 3)
-		{	
-			while(choice != 5){
-				displayMenu();
-				choice = getInput();
-				std::cin.ignore();
-				int index;
-				
-				if(choice == 1)
-				{
-					wallet.displayPrivateKey();
-				} else if(choice == 2) 
-				{
-					wallet.displayMnemonic();
-					std::cout << "\n" << std::endl;
-
-				} else if (choice == 3)
-				{
-					index = getInput();
-					wallet.displayChildPrivateKey(index);
-
-				} else if (choice == 4)
-				{
-					index = getInput();
-					wallet.displayAddress(index);
-				}
-			}
-		}
-	}
-}
