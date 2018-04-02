4db130ff1077069220b65e666d6ddc7158e99f77
AaronJaramillo <aajaramillo95@gmail.com>
2018-04-02 18:15:28 +0700

    Address_Subscribe Tutorial added

--- rawTX/address_subscribe.cpp
+++ rawTX/address_subscribe.cpp
@@ -8,30 +8,29 @@ using namespace bc;
 void getTXInfo(client::connection_type connection, const hash_digest tx_hash, wallet::payment_address watch_address) {
 	client::obelisk_client client(connection);
 
-    auto on_done = [&watch_address](const chain::transaction tx)
-    {
-    	for (chain::output out: tx.outputs())
-    	{	
-    		// std::cout << wallet::payment_address(out.address().hash(), wallet::payment_address::testnet_p2kh).encoded() <<std::endl;
-    		if (wallet::payment_address(out.address().hash(), wallet::payment_address::testnet_p2kh) == watch_address)
-    			std::cout << "Transaction value: "<< encode_base10(out.value(), 8) << std::endl;
+	auto on_done = [&watch_address](const chain::transaction tx)
+	{
+		for (chain::output out: tx.outputs())
+		{	
+			// std::cout << wallet::payment_address(out.address().hash(), wallet::payment_address::testnet_p2kh).encoded() <<std::endl;
+			if (wallet::payment_address(out.address().hash(), wallet::payment_address::testnet_p2kh) == watch_address)
+				std::cout << "Transaction value: "<< encode_base10(out.value(), 8) << std::endl;
 
-    	}
-    	   
-    };
+		}
+	};
 
-    auto on_error = [](const code& ec)
-    {
-    	std::cout << "Error Code: " << ec.message() << std::endl;
-    };
+	auto on_error = [](const code& ec)
+	{
+		std::cout << "Error Code: " << ec.message() << std::endl;
+	};
 
 	if(!client.connect(connection))
 	{
 		std::cout << "Fail" << std::endl;
 	}
 
-    client.transaction_pool_fetch_transaction(on_error, on_done, tx_hash);
-    client.wait();
+	client.transaction_pool_fetch_transaction(on_error, on_done, tx_hash);
+	client.wait();
 }
 
 void subscribe(wallet::payment_address watch_address) {
@@ -52,13 +51,12 @@ void subscribe(wallet::payment_address watch_address) {
 
 	};
 
-    auto on_update = [&connection, &watch_address](const code& error,
-        uint16_t sequence, size_t height, const hash_digest& tx_hash)
-    {
-    	std::cout << "New Transaction Recieved: " << std::endl;
-	    std::cout << sequence <<": "<< "Hash: " << encode_hash(tx_hash) <<" Confirmed: "<< height << std::endl;
-	    getTXInfo(connection, tx_hash, watch_address);
-    };
+	auto on_update = [&connection, &watch_address](const code& error, uint16_t sequence, size_t height, const hash_digest& tx_hash)
+	{
+		std::cout << "New Transaction Recieved: " << std::endl;
+		std::cout << sequence <<": "<< "Hash: " << encode_hash(tx_hash) <<" Confirmed: "<< height << std::endl;
+		getTXInfo(connection, tx_hash, watch_address);
+	};
 
 	if(!client.connect(connection))
 	{
